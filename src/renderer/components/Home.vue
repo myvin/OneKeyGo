a0d3c62ba565d5ac0c11323a4284da064fdf66d8
myvin <851399101@qq.com>
2018-11-30 21:07:31 +0800

    remove useless codes

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -18,20 +18,7 @@
       return {}
     },
     mounted () {},
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
-      },
-      notify () {
-        let notification = new Notification('title', {
-          body: 'content',
-          icon: '/static/weather.png'
-        })
-        notification.onclick = () => {
-          console.log('通知被点击')
-        }
-      }
-    }
+    methods: {}
   }
 </script>
 
