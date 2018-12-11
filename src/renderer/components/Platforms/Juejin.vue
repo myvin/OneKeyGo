20fca9dfac7050b2f9536a3bae210fb18c8b4735
myvin <851399101@qq.com>
2018-12-11 16:57:50 +0800

    update pages

--- src/renderer/components/Platforms/Juejin.vue
+++ src/renderer/components/Platforms/Juejin.vue
@@ -31,13 +31,7 @@
       </el-form-item>
       <el-form-item>
         <el-button-group>
-          <el-button size='small' type="primary" @click="validate('refName')" icon='el-icon-question'>验证账号</el-button>
-          <el-button size='small' type="success" @click="create('refName')" icon='el-icon-circle-check'>创建/更新账号信息</el-button>
-        </el-button-group>
-      </el-form-item>
-      <el-form-item>
-        <el-button-group>
-          <el-button size='small' type='warning' @click="resetForm('refName')" icon='el-icon-circle-close'>重置表单</el-button>
+          <el-button size='small' type="primary" @click="create('refName')" icon='el-icon-success'>保存/更新本地账号信息</el-button>
           <el-button size='small' type='danger' @click="remove" icon='el-icon-delete'>删除本地账号信息</el-button>
         </el-button-group>
       </el-form-item>
@@ -57,12 +51,10 @@
         },
         rules: {
           account: [
-            { required: true, message: '请输入手机号或邮箱', trigger: 'blur' },
-            { min: 3, max: 50, message: '长度在 3 到 50 个字符', trigger: 'blur' }
+            { required: true, message: '请输入手机号或邮箱', trigger: 'blur' }
           ],
           password: [
-            { required: true, message: '请输入密码', trigger: 'blur' },
-            { min: 3, max: 50, message: '长度在 3 到 50 个字符', trigger: 'blur' }
+            { required: true, message: '请输入密码', trigger: 'blur' }
           ]
         }
       }
@@ -72,9 +64,9 @@
     },
     methods: {
       readDb () {
-        this.juejin = this.$db.getState().platforms.juejin
+        this.juejin = this.$db.read().get('platforms.juejin')
       },
-      async login (create) {
+      async login () {
         let loading = this.$loading({
           lock: true,
           text: '努力加载中...',
@@ -86,93 +78,59 @@
             phoneNumber: this.form.account,
             password: this.form.password
           })
-          if (create) {
-            const juejin = {
-              token: d.token,
-              userId: d.userId,
-              clientId: d.clientId
-            }
-            this.juejin = juejin
-            this.$db.set('platforms.juejin', juejin).write()
+          const juejin = {
+            token: d.token,
+            userId: d.userId,
+            clientId: d.clientId
           }
-          let notification = new this.$electron.remote.Notification({
-            title: create ? '创建结果' : '验证结果',
-            body: create ? '账号信息创建/更新成功！' : '账号信息正确'
+          this.juejin = juejin
+          this.$db.set('platforms.juejin', juejin).write()
+          this.$notify({
+            title: '提示',
+            message: '账号信息保存/更新成功!',
+            type: 'success'
           })
-          notification.show()
-          notification.onclick = () => {
-            notification.close()
-          }
         } catch (err) {
-          let notification
           const status = err.response.status
           if (status === 401) {
-            notification = new this.$electron.remote.Notification({
+            this.$notify({
               title: '信息有误',
-              body: '密码错误，请重新填写'
+              message: '密码错误，请重新填写!',
+              type: 'error'
             })
           } else if (status === 404) {
-            notification = new this.$electron.remote.Notification({
+            this.$notify({
               title: '信息有误',
-              body: '账号不存在，请重新填写'
+              message: '账号不存在，请重新填写!',
+              type: 'error'
             })
-          }
-          notification.show()
-          notification.onclick = () => {
-            notification.close()
           }
         }
         loading.close()
       },
-      validate (formName) {
+      create (formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
             this.login()
           } else {
-            let notification = new this.$electron.remote.Notification({
+            this.$notify({
               title: '提示',
-              body: '请填写必填选项'
+              message: '请填写必填选项',
+              type: 'info'
             })
-            notification.show()
-            notification.onclick = () => {
-              notification.close()
-            }
             return false
           }
         })
-      },
-      create (formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.login('create')
-          } else {
-            let notification = new this.$electron.remote.Notification({
-              title: '提示',
-              body: '请填写必填选项'
-            })
-            notification.show()
-            notification.onclick = () => {
-              notification.close()
-            }
-            return false
-          }
-        })
-      },
-      resetForm (formName) {
-        this.$refs[formName].resetFields()
       },
       remove () {
         const juejin = {}
         this.juejin = juejin
         this.$db.set('platforms.juejin', juejin).write()
-        let notification = new this.$electron.remote.Notification({
+        this.$notify({
           title: '删除提示',
-          body: '本地账号信息删除完成'
+          message: '本地账号信息删除完成',
+          type: 'success'
         })
-        notification.show()
-        notification.onclick = () => {
-          notification.close()
-        }
       }
     }
   }
--- src/renderer/components/Platforms/SegmentFault.vue
+++ src/renderer/components/Platforms/SegmentFault.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="segmentFault">
     <div class='formtitle'>{{$route.meta.label}}平台设置</div>
-    <el-form label-position='left' ref="form" :model="segmentFault" :rules='rules' label-width="7em" size='small'>
-      <el-form-item prop='PHPSESSID'>
+    <el-form label-position='left' ref="form" :model="form" :rules='rules' label-width="7em" size='small'>
+      <el-form-item label="账号" prop='account'>
+        <el-input placeholder='请输入手机号或邮箱' v-model="form.account"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop='password'>
+        <el-input placeholder='请输入密码' v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
         <template slot='label'>PHPSESSID</template>
         <template>
-          <el-input placeholder='PHPSESSID' v-model='segmentFault.PHPSESSID'></el-input>
-          <i class='el-icon-info tip'></i> <span class='tip'>复制 cookie 中的 PHPSESSID</span>
+          <el-input placeholder='PHPSESSID' :disabled='true' v-model='segmentFault.PHPSESSID'></el-input>
+          <i class='el-icon-info tip'></i> <span class='tip'>服务端生成的 SESSIONID</span>
         </template>
       </el-form-item>
-      <el-form-item prop='token'>
-        <template slot='label'>token</template>
-        <template>
-          <el-input placeholder='token' v-model='segmentFault.token'></el-input>
-          <i class='el-icon-info tip'></i> <span class='tip'>登录网页版后，随便打开一个页面，查看页面源码，找到 w.SF.token function，执行该 function 后返回的字符串作为 token</span>
-        </template>
-      </el-form-item>
-      <el-form-item prop='tags'>
+      <el-form-item>
         <template slot='label'>标签</template>
         <template>
-          <el-select style='width: 100%;'
+          <el-select @change='tagsChange' style='width: 100%;'
             :disabled='disabled'
-            v-model="segmentFault.tags"
+            v-model="tags"
             multiple
             :multiple-limit='5'
             filterable
@@ -33,8 +32,10 @@
             <el-option
               v-for="item in searchResults"
               :key="item.id"
-              :label="item.name"
-              :value="item.name">
+              :label="item.name + ',' + item.id"
+              :value="item.id">
+              <span style="float: left">{{item.name}}</span>
+              <span style="float: right; color: #8492a6; font-size: 13px">tagId: {{item.id}}</span>
             </el-option>
           </el-select>
           <i class='el-icon-info tip'></i> <span class='tip'>发布文章时必填项；需先填写账号信息方可搜索。</span>
@@ -42,30 +43,42 @@
       </el-form-item>
       <el-form-item>
         <el-button-group>
-          <el-button size='small' type="primary" @click="save('form')" icon='el-icon-success'>确定</el-button>
+          <el-button size='small' type="primary" @click="save('form')" icon='el-icon-success'>保存/更新本地账号信息</el-button>
           <el-button size='small' type='danger' @click="remove" icon='el-icon-delete'>删除本地账号信息</el-button>
         </el-button-group>
       </el-form-item>
     </el-form>
+    <el-alert
+      title="token 参数"
+      description="在发送 API 请求时，还有一个必须参数 token，token 需要在页面内动态获取，不过不用担心，OneKeyGo 已经帮你做了。感兴趣的可以打开 segmentfault 任意页面，在源代码中搜索 w.SF.token，这个 function 就是用来生成 token 的。"
+      type="info"
+      show-icon
+      :closable="false">
+    </el-alert>
   </div>
 </template>
 
 <script>
-  import { search } from '../../../utils/segmentFaultPublish'
+  import { getWriteToken, login, search } from '../../../utils/segmentFaultPublish'
 
   export default {
     data () {
       return {
+        tags: [],
+        tagsLen: 0,
+        tagsToSave: [],
+        writeToken: '',
         segmentFault: {},
+        form: {
+          account: '',
+          password: ''
+        },
         rules: {
-          PHPSESSID: [
-            { required: true, message: '请输入 PHPSESSID', trigger: 'blur' }
+          account: [
+            { required: true, message: '请输入手机号或邮箱', trigger: 'blur' }
           ],
-          token: [
-            { required: true, message: '请输入 token', trigger: 'blur' }
-          ],
-          tags: [
-            { type: 'array', required: true, message: '至少填写一个标签', trigger: 'blur' }
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' }
           ]
         },
         loading: false,
@@ -75,8 +88,7 @@
     computed: {
       disabled () {
         let PHPSESSID = this.segmentFault.PHPSESSID
-        let token = this.segmentFault.token
-        if (!PHPSESSID || !PHPSESSID.replace(/\s+/g, '') || !token || !token.replace(/\s+/g, '')) {
+        if (!PHPSESSID || !PHPSESSID.replace(/\s+/g, '')) {
           return true
         }
         return false
@@ -87,48 +99,130 @@
     },
     methods: {
       readDb () {
-        this.segmentFault = Object.assign({}, this.$db.getState().platforms.segmentFault)
+        this.segmentFault = Object.assign({}, this.$db.read().get('platforms.segmentFault').value())
+        this.tagsToSave = this.segmentFault.tags || []
+        this.tags = (this.segmentFault.tags || []).map(item => {
+          return `${item.name},${item.id}`
+        })
+        this.tagsLen = this.tags.length
+      },
+      login () {
+        let loading = this.$loading({
+          lock: true,
+          text: '努力加载中...',
+          spinner: 'el-icon-loading',
+          background: 'rgba(0, 0, 0, 0.7)'
+        })
+        login({
+          remember: 1,
+          username: this.form.account,
+          password: this.form.password
+        }, (res, token, PHPSESSID) => {
+          if (res.status === 0) {
+            this.writeSession(PHPSESSID)
+          } else {
+            this.$notify({
+              title: '信息有误',
+              message: JSON.stringify(res.data[1]),
+              type: 'error'
+            })
+          }
+        }, (err, token, PHPSESSID) => {
+          console.log('login catch err: ', err)
+          if (err.statusCode === 302) {
+            this.writeSession(PHPSESSID)
+            return
+          }
+          this.$notify({
+            title: '提示',
+            message: err.message || '登录失败，请稍后重试',
+            type: 'error'
+          })
+        })
+        loading.close()
+      },
+      writeSession (PHPSESSID) {
+        this.segmentFault = {
+          PHPSESSID
+        }
+        this.$db.set('platforms.segmentFault.PHPSESSID', PHPSESSID).write()
+        this.$notify({
+          title: '提示',
+          message: '账号信息保存/更新成功!',
+          type: 'success'
+        })
       },
       save (formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.$db.set('platforms.segmentFault', this.segmentFault).write()
-            this.notification({
+        if (!this.segmentFault.PHPSESSID) {
+          this.$refs[formName].validate((valid) => {
+            if (valid) {
+              this.$db.set('platforms.segmentFault', this.segmentFault).write()
+              this.login()
+            } else {
+              this.$notify({
+                title: '提示',
+                message: '请填写必填选项',
+                type: 'info'
+              })
+              return false
+            }
+          })
+        } else {
+          if (this.tags.length) {
+            this.$db.set('platforms.segmentFault.tags', this.tagsToSave).write()
+            this.$notify({
               title: '提示',
-              body: '保存成功!'
+              message: '文章标签保存成功!',
+              type: 'success'
             })
           } else {
-            return false
+            this.$notify({
+              title: '提示',
+              message: '请填写文章标签',
+              type: 'info'
+            })
           }
-        })
+        }
       },
       remove () {
         this.segmentFault = {}
         this.$db.set('platforms.segmentFault', {}).write()
-        this.notification({
+        this.$notify({
           title: '删除提示',
-          body: '本地账号信息删除完成!'
+          message: '本地账号信息删除完成!',
+          type: 'success'
         })
       },
-      notification ({
-        title,
-        body
-      }) {
-        let notification = new this.$electron.remote.Notification({
-          title,
-          body
-        })
-        notification.show()
-        notification.onclick = () => {
-          notification.close()
+      tagsChange (ids) {
+        ids = ids || []
+        let arr = []
+        if (this.tagsLen > this.tags.length) {
+          // 删除了标签
+          for (let i = 0, len = ids.length; i < len; i++) {
+            let obj = this.tagsToSave.find(item => {
+              return item.id === ids[i].split(',')[1]
+            })
+            arr.push(obj)
+          }
+          this.tagsToSave = arr
+        } else if (this.tagsLen < this.tags.length) {
+          // 添加了标签
+          let obj = this.searchResults.find(item => {
+            return item.id === ids[ids.length - 1]
+          })
+          this.tagsToSave.push(obj)
         }
+        this.tagsLen = this.tags.length
       },
       async search (query) {
+        if (!this.writeToken) {
+          this.writeToken = await getWriteToken()
+        }
         if (query !== '') {
           this.loading = true
           try {
             const data = await search({
-              token: this.segmentFault.token,
+              token: this.writeToken,
               PHPSESSID: this.segmentFault.PHPSESSID,
               q: query
             })
@@ -136,16 +230,19 @@
               this.loading = false
               this.searchResults = data.data
             } else {
-              this.notification({
+              this.$notify({
                 title: '搜索标签',
-                body: '搜索标签发生错误'
+                message: '搜索标签发生错误!',
+                type: 'error'
               })
             }
           } catch (err) {
+            console.log('search catch err: ', err)
             if (err.statusCode === 404) {
-              this.notification({
+              this.$notify({
                 title: '提示',
-                body: '请核对账号信息是否正确!'
+                message: '请核对账号信息是否正确!',
+                type: 'error'
               })
             }
           }
